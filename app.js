bec9af535125aabafc4c892d1e2667056f8fc3a0
agarcian031 <algarcian031@outlook.com>
2019-01-30 23:45:21 -0600

    Update app.js with celsius formula

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector(".temperature-description"); 
     let temperatureDegree = document.querySelector(".temperature-degree"); 
     let locationTimezone = document.querySelector(".location-timezone"); 
+    let temperatureSection = document.querySelector(".temperature"); 
+    const temperatureSpan = document.querySelector(".temperature span"); 
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -26,9 +29,23 @@ window.addEventListener('load', () => {
                 temperatureDegree.textContent = temperature; 
                 temperatureDescription.textContent = summary;  
                 locationTimezone.textContent = data.timezone; 
+
+                //formula for celsius 
+                let celsius = (temperature - 32) * (5 / 9); 
                 //set Icon 
                 setIcons(icon, document.querySelector('.icon')); 
-            
+                
+                //Change temperature Celsius 
+                temperatureSection.addEventListener('click', () => {
+                    if (temperatureSpan.textContent === "F") {
+                        temperatureSpan.textContent = "C"; 
+                        temperatureDegree.textContent = Math.floor(celsius); 
+                    } 
+                    else {
+                        temperatureSpan.textContent = "F"; 
+                        temperatureDegree.textContent = temperature; 
+                    } //end if/else 
+                }); //end temperature.Section.addEventListener
             }); //end .then(data)
         }); 
 
